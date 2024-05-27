ced05e9925df4dcdd6a1cf3e67f1817064158408
sripadsirik <sripadsirik@gmail.com>
2024-05-27 12:28:37 -0500

    disabled the back button on android

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BackHandler } from 'react-native';
 import './firebase.js';
 
 import RootStack from './navigators/RootStack'
 
 export default function App() {
+  useEffect(() => {
+    const handleBackButton = () => true;
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+    };
+  }, []);
+
   return <RootStack />
 }
