1d631b99bc4191e013aab6af690445fda5047ca7
Sri Sirikonda <ssiri5@uic.edu>
2024-08-14 16:24:48 -0500

    ejf

--- temp.js
+++ temp.js
@@ -10,7 +10,6 @@ import FlashMessage, { showMessage } from "react-native-flash-message";
 import { Audio } from 'expo-av';
 import { CommonActions } from '@react-navigation/native';
 import { getFirestore, collection, where, getDocs, doc, setDoc, deleteDoc, query, getDoc } from "firebase/firestore";
-import mathawelcome from '../matha screens/mathawelcome';
 import {
     InnerContainer,
     PageLogo,
@@ -40,7 +39,6 @@ const Welcome = ({ navigation }) => {
     const [selectedFamily, setSelectedFamily] = useState(null);
     const [isPickerVisible, setPickerVisible] = useState(false);
     const [pickerValue, setPickerValue] = useState('');
-    const [isMatha, setIsMatha] = useState(false);
     const formikRef = useRef();
 
     const toggleSound = async () => {
@@ -130,37 +128,6 @@ const Welcome = ({ navigation }) => {
         setIsHeadOfFamily(true);
     };
 
-    const handleMathaConfirmation = async () => {
-      try {
-          if (familyName) {
-              // Reference to the family document
-              const familyDocRef = doc(db, "families", familyName);
-              
-              // Fetch the current family document
-              const familyDoc = await getDoc(familyDocRef);
-              
-              if (familyDoc.exists()) {
-                  // Update the family document to set the user as a matha
-                  await setDoc(familyDocRef, {
-                      matha: userEmail  // Add the matha field with the user's email
-                  }, { merge: true });
-  
-                  // Update the state to reflect the matha status
-                  setIsMatha(true);
-                  Alert.alert("Matha Confirmed", "You are confirmed as Matha of the family!");
-                  navigation.navigate("mathawelcome");
-              } else {
-                  Alert.alert("Error", "Family document does not exist.");
-              }
-          } else {
-              Alert.alert("Error", "No family found to update.");
-          }
-      } catch (error) {
-          console.error("Error updating family document: ", error);
-          Alert.alert("Error", "There was an error updating the family document. Please try again.");
-      }
-  };
-  
     const fetchFamilies = async () => {
         try {
             const familyCollection = collection(db, "families");
@@ -279,30 +246,29 @@ const Welcome = ({ navigation }) => {
                             <SubTitle welcome={true}>
                                 {familyName ? `You are part of the family: ${familyName}` : "You are not part of any family.\n Please answer questions below"}
                             </SubTitle>
+                            {/* <Line /> */}
                             <StyledFormArea>
                                 <Avatar resizeMode="cover" source={require('../assets/img1.webp')} />
                                 <Line />
-                                {!familyName && !isHeadOfFamily && !isMatha && (
+                                {!familyName && !isHeadOfFamily && (
                                     <View>
                                         <Text>Are you the head Swami of the family?</Text>
                                         <Button title="Yes" onPress={handleHeadOfFamilyConfirmation} />
                                         <Text> </Text>
-                                        <Button title="No" onPress={() => setIsChoosingFamily(true)} />
+                                        <Button title="No" onPress={fetchFamilies} />
                                     </View>
                                 )}
-
-                                {isHeadOfFamily && (
+                                {isHeadOfFamily && !familyName && (
                                     <Formik
+                                        innerRef={formikRef}
                                         initialValues={{ fullName: '' }}
                                         validationSchema={validationSchema}
-                                        innerRef={formikRef}
                                         onSubmit={handleCreateFamily}
                                     >
                                         {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
                                             <View>
-                                                <Text> </Text>
                                                 <TextInput
-                                                    placeholder="Enter Family Name"
+                                                    placeholder="Family Name"
                                                     onChangeText={handleChange('fullName')}
                                                     onBlur={handleBlur('fullName')}
                                                     value={values.fullName}
@@ -311,54 +277,12 @@ const Welcome = ({ navigation }) => {
                                                 {errors.fullName && touched.fullName && (
                                                     <Text style={styles.errorText}>{errors.fullName}</Text>
                                                 )}
-                                                <Button title="Submit" onPress={handleSubmit} />
+                                                <StyledButton onPress={handleSubmit}>
+                                                    <ButtonText>Create Family</ButtonText>
+                                                </StyledButton>
                                             </View>
                                         )}
                                     </Formik>
-                                )}
-
-                                {isChoosingFamily && !isHeadOfFamily && (
-                                    <View>
-                                        <Button title="Select Existing Family" onPress={fetchFamilies} />
-                                    </View>
-                                )}
-
-                                
-                                <Modal
-                                                visible={isPickerVisible}
-                                                transparent={true}
-                                                animationType="slide"
-                                                onRequestClose={() => setPickerVisible(false)}
-                                            >
-                                                <View style={styles.modalContainer}>
-                                                    <View style={styles.modalContent}>
-                                                        <Text>Select a Family</Text>
-                                                        <Picker
-                                                            selectedValue={pickerValue}
-                                                            onValueChange={(itemValue) => setPickerValue(itemValue)}
-                                                            style={styles.picker}
-                                                        >
-                                                            <Picker.Item label="Select a family" value="" />
-                                                            {families.map(family => (
-                                                                <Picker.Item key={family.value} label={family.label} value={family.value} />
-                                                            ))}
-                                                        </Picker>
-                                                        <Button title="Join Family" onPress={handleJoinFamily} />
-                                                        <Button title="Cancel" onPress={() => setPickerVisible(false)} />
-                                                    </View>
-                                                </View>
-                                            </Modal>
-
-                                {familyName && !isMatha && (
-                                    <View>
-                                        <Text>Are you a Matha of the family?</Text>
-                                        <Button title="Yes" onPress={handleMathaConfirmation} />
-                                        <Button title="No" onPress={() => {}} />
-                                    </View>
-                                )}
-
-                                {isMatha && (
-                                    <Text>You are confirmed as Matha of the family!</Text>
                                 )}
                             </StyledFormArea>
                         </WelcomeContainer>
@@ -366,10 +290,36 @@ const Welcome = ({ navigation }) => {
                         <Text> </Text>
                         <Text> </Text>
                         <Text> </Text>
-                        <Text> </Text>
                     </InnerContainer>
                 </StyledContainer>
             </ScrollView>
+            <Modal
+                visible={isPickerVisible}
+                transparent={true}
+                animationType="slide"
+                onRequestClose={() => setPickerVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text>Select a Family</Text>
+                        <Picker
+                            selectedValue={pickerValue}
+                            onValueChange={(itemValue) => setPickerValue(itemValue)}
+                            style={styles.picker}
+                        >
+                            <Picker.Item label="Select a family" value="" />
+                            {families.map(family => (
+                                <Picker.Item key={family.value} label={family.label} value={family.value} />
+                            ))}
+                        </Picker>
+                        <Button title="Join Family" onPress={handleJoinFamily} />
+                        <Text> </Text>
+                        <Button title="Cancel" onPress={() => setPickerVisible(false)} />
+                    </View>
+                </View>
+            </Modal>
+            <FlashMessage position="top" />
+            
         </KeyboardAvoidingView>
     );
 };
@@ -379,17 +329,29 @@ const styles = StyleSheet.create({
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
-        marginBottom: 10,
-        paddingHorizontal: 10,
+        marginBottom: 12,
+        paddingHorizontal: 8,
+        borderRadius: 4
     },
     errorText: {
         color: 'red',
+        marginBottom: 12,
     },
     modalContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        width: '80%',
+    },
+    picker: {
+        height: 200,
+        width: '100%',
     },
 });
 
