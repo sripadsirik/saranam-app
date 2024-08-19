c44a3337b846052f3f340bcc709887bf62bb55c9
Sri Sirikonda <ssiri5@uic.edu>
2024-08-19 18:16:13 -0500

    added delete

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, Alert, Button, TextInput, KeyboardAvoidingView, ScrollView, Platform, StyleSheet, Modal } from 'react-native';
+import { View, Text, Alert, Button, TextInput, KeyboardAvoidingView, ScrollView, Platform, StyleSheet, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { StatusBar } from 'expo-status-bar';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { auth, db } from '../firebase';
-import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { signOut, deleteUser, onAuthStateChanged } from 'firebase/auth';
 import FlashMessage, { showMessage } from "react-native-flash-message";
 import { Audio } from 'expo-av';
 import { CommonActions } from '@react-navigation/native';
 import { getFirestore, collection, where, getDocs, doc, setDoc, deleteDoc, query, getDoc, updateDoc } from "firebase/firestore";
+import { Icon } from 'react-native-elements';
+import Modal from 'react-native-modal';
+
 import {
     InnerContainer,
-    PageLogo,
     PageTitle,
     SubTitle,
     StyledFormArea,
@@ -41,8 +43,9 @@ const Welcome = ({ navigation }) => {
     const [pickerValue, setPickerValue] = useState('');
     const [isMatha, setIsMatha] = useState(false);
     const [showMathaForm, setShowMathaForm] = useState(true);
-    const [hasSelectedMathaOption, setHasSelectedMathaOption] = useState(false);  
+    const [hasSelectedMathaOption, setHasSelectedMathaOption] = useState(false);
     const formikRef = useRef();
+    const [isModalVisible, setModalVisible] = useState(false); // State to control modal visibility
 
     const toggleSound = async () => {
         if (isPlaying) {
@@ -133,6 +136,46 @@ const Welcome = ({ navigation }) => {
         } catch (error) {
             console.error('Error signing out:', error);
         }
+    };
+
+    const handleDeleteAccount = async () => {
+        Alert.alert(
+            'Delete Account',
+            'Are you sure you want to delete your account? This action cannot be undone.',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    style: 'destructive',
+                    onPress: async () => {
+                        try {
+                            const user = auth.currentUser;
+                            if (user) {
+                                await deleteUser(user);
+                                toggleSound(); // Stop the music if account is deleted
+                                console.log('User account deleted');
+                                Alert.alert('Account Deleted', 'Your account has been deleted successfully.');
+
+                                navigation.dispatch(
+                                    CommonActions.reset({
+                                        index: 0,
+                                        routes: [{ name: 'Login' }],
+                                    })
+                                );
+                            } else {
+                                console.log('No user is signed in');
+                            }
+                        } catch (error) {
+                            console.error('Error deleting user account:', error);
+                            Alert.alert('Error', 'There was an issue deleting your account. Please try again later.');
+                        }
+                    },
+                },
+            ]
+        );
     };
 
     const handleHeadOfFamilyConfirmation = () => {
@@ -301,14 +344,41 @@ const Welcome = ({ navigation }) => {
                 <StyledContainer>
                     <StatusBar style="dark" />
                     <InnerContainer>
+                        <TouchableOpacity
+                            style={{ 
+                                position: 'absolute', 
+                                top: 50, 
+                                right: 10, 
+                                zIndex: 10, // Ensure the touchable is on top
+                                backgroundColor: 'transparent', // Makes sure the touchable area is clear
+                                padding: 5 // Adds padding to make it easier to tap
+                            }}
+                            onPress={() => setModalVisible(true)}
+                        >
+                            <Icon name="settings" size={30} color="#000" />
+                        </TouchableOpacity>
+
+                        <Modal
+                            isVisible={isModalVisible}
+                            onBackdropPress={() => setModalVisible(false)} // Close modal on backdrop press
+                            style={{ justifyContent: 'flex-end', margin: 0 }} // Position at bottom
+                        >
+                            <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+                                <TouchableOpacity onPress={handleSignOut} style={{ padding: 10 }}>
+                                    <Text>Logout</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={handleDeleteAccount} style={{ padding: 10 }}>
+                                    <Text>Delete Account</Text>
+                                </TouchableOpacity>
+                                <Button title="Close" onPress={() => setModalVisible(false)} />
+                            </View>
+                        </Modal>
+
                         <Text> </Text>
                         <Text> </Text>
                         <Text> </Text>
                         <Text> </Text>
                         <WelcomeContainer>
-                            <StyledButton onPress={handleSignOut} style={styles.redButton}>
-                                <ButtonText>Logout</ButtonText>
-                            </StyledButton>
                             <StyledButton onPress={handleResetFamily}>
                                 <ButtonText>Reset Family/Form</ButtonText>
                             </StyledButton>
@@ -378,7 +448,7 @@ const Welcome = ({ navigation }) => {
                                     <>
                                         <Text>You are confirmed as Matha of the family!</Text>
                                         <StyledButton onPress={() => navigation.navigate("MathaTabs")}>
-                                            <ButtonText>Click to Matha Screens to Select Food</ButtonText>
+                                            <ButtonText>Go to Matha Screens to Select Food</ButtonText>
                                         </StyledButton>
                                     </>
                                 )}
